68ebc7defc4cf79e514eb4ef2e2459efc4e6f529
sc0Vu <alk03073135@gmail.com>
2017-12-01 07:19:37 +0800

    Add fromPrivateKey

--- test/wallet.js
+++ test/wallet.js
@@ -4,7 +4,9 @@ const yoethwallet = require('../index');
 
 tape('yoethwallet wallet test', (t) => {
   const wallet = yoethwallet.wallet;
-  const walletJson = '{"version":3,"id":"14c3ce7b-137d-4e7f-b7ae-addcc463b855","address":"c4a7d55f6bc573cfb72dd04f8b3e84ca6789a2ea","crypto":{"ciphertext":"3aa0bb0578a9c1b0af5f30e05d73c2536bba515e3e488c1fe34c1334a339c3ae","cipherparams":{"iv":"eebcd53b12a0e7975ec101ad5e7d95ab"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"262a812d44d7bf27d99f58e134e6b97a8194e818a3ae73cdb12eb7e74b6ed264","n":262144,"r":8,"p":1},"mac":"67902a90cc0e5b5a8eefea810a37c5347d8009575ef89c5a15776f75150d558f"}}';
+  let walletJson = '';
+  let address = '';
+  let privateKey = '';
 
   wallet.generate();
 
@@ -20,7 +22,9 @@ tape('yoethwallet wallet test', (t) => {
   });
 
   t.test('sholud get V3 json with default scrypt and aes-128-ctr', (st) => {
-    const v3 = JSON.parse(wallet.toJson('123456'));
+    walletJson = wallet.toJson('123456');
+
+    const v3 = JSON.parse(walletJson);
 
     st.equal(v3.version, 3);
     st.equal(v3.crypto.cipher, 'aes-128-ctr');
@@ -48,7 +52,9 @@ tape('yoethwallet wallet test', (t) => {
   });
 
   t.test('should get private key buffer', (st) => {
-    st.equal(wallet.getPrivateKeyBuffer().toString('hex'), wallet.getPrivateKey());
+    privateKey = wallet.getPrivateKeyBuffer();
+
+    st.equal(privateKey.toString('hex'), wallet.getPrivateKey());
     st.end();
   });
 
@@ -58,7 +64,9 @@ tape('yoethwallet wallet test', (t) => {
   });
 
   t.test('should get address', (st) => {
-    st.equal(/^0x[a-fA-F0-9]{40}$/.test(wallet.getAddress()), true);
+    address = wallet.getAddress();
+
+    st.equal(/^0x[a-fA-F0-9]{40}$/.test(address), true);
     st.end();
   });
 
@@ -66,7 +74,15 @@ tape('yoethwallet wallet test', (t) => {
     const v3Wallet = yoethwallet.wallet;
 
     v3Wallet.fromJson(walletJson, '123456');
-    st.equal(v3Wallet.getAddress(), '0xc4a7d55f6bc573cfb72dd04f8b3e84ca6789a2ea');
+
+    st.equal(v3Wallet.getAddress(), address);
+    st.end();
+  });
+
+  t.test('should generate from private key', (st) => {
+    const pkWallet = wallet.fromPrivateKey(privateKey);
+
+    st.equal(pkWallet.getAddress(), address);
     st.end();
   });
 
